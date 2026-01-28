6461409f4d63acff5a45b11883f7707da327b816
Quantum-Leap17 <pahujaparv2007@gmail.com>
2026-01-28 21:47:35 +0530

    Initial Commit

---
+++ Personal Portfolio/script.js
@@ -0,0 +1,6 @@
+// Simple form submit handler
+document.querySelector(".contact-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Thank you! Your message has been sent.");
+    this.reset();
+});
